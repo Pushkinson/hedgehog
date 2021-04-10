40f60a31b9f390c21557fcda6915f409f4631741
Udovik Aleksey <udovik.aleksey@mail.ru>
2021-04-10 15:37:49 +0300

    header, main, content, slider for pc

---
+++ app/js/main.js
@@ -0,0 +1,41 @@
+(function() {
+  const navigationCatalog = document.querySelector('.navigation__catalog'),
+        navigationList    = document.querySelector('.navigation__list--hide'),
+        navigationArrow   = document.querySelector('.navigation__arrow');
+
+  navigationCatalog.addEventListener('click', () => {
+    navigationList.classList.toggle('navigation__list--active');
+    navigationArrow.classList.toggle('navigation__arrow--active');
+  });
+
+  const sliderLeftArrow   = document.querySelector('.slider__arrow-left'),
+        sliderRightArrow  = document.querySelector('.slider__arrow-right'),
+        slider            = document.querySelector('.slider__frame'),
+        sliderItems       = document.querySelector('.slider__item');
+  let   slideNumber       = 0,
+        shiftPosition     = 0;
+
+  sliderRightArrow.addEventListener('click', function() {
+
+    if (slideNumber === 3) {
+        return;
+    }
+
+    shiftPosition += -(sliderItems.offsetWidth + 40);
+    slider.style.transform = `translateX(${shiftPosition}px)`;
+    slideNumber ++;
+  });
+
+  sliderLeftArrow.addEventListener('click', function() {
+
+    if (slideNumber === 0) {
+        return;
+    }
+
+    shiftPosition += (sliderItems.offsetWidth + 40);
+    slider.style.transform = `translateX(${shiftPosition}px)`;
+    slideNumber --;
+  });
+
+
+}());
